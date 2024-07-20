639acfeca5475b273026c3604fc357e6566c18f3
pietro.marilei <pietromrl0@gmail.com>
2024-07-20 14:27:49 +0200

    add log in e cards page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CounterComponent } from './counter/counter.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'counter', component: CounterComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
