6a150447854656e108bd0b5fb0d932020bbeb568
pietro.marilei <pietromrl0@gmail.com>
2024-08-05 17:04:13 +0200

    add error management fe

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule], // Aggiungi CommonModule
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  errorMessage: string | null = null; // Aggiungi una variabile per l'errore
+  errorDetails: any = null; // Aggiungi una variabile per i dettagli dell'errore
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -32,6 +35,30 @@ export class LoginComponent {
         'Errore durante il login:',
         error.response?.data || error.message || error
       );
+      this.errorMessage =
+        error.response?.data?.detail || 'Errore durante il login'; // Imposta il messaggio di errore
     }
+  }
+
+  redirectToRegister() {
+    this.router.navigate(['/register']); // Redirect alla pagina /register
+  }
+  getErrorDetails(): string[] {
+    if (!this.errorDetails) {
+      return [];
+    }
+
+    const details: string[] = [];
+    for (const key in this.errorDetails) {
+      if (this.errorDetails.hasOwnProperty(key)) {
+        const value = this.errorDetails[key];
+        if (Array.isArray(value)) {
+          details.push(...value);
+        } else {
+          details.push(value);
+        }
+      }
+    }
+    return details;
   }
 }
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import axios from 'axios';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule], // Aggiungi CommonModule
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
@@ -15,6 +16,8 @@ export class RegisterComponent {
   email: string = '';
   username: string = '';
   password: string = '';
+  errorMessage: string | null = null; // Aggiungi una variabile per l'errore
+  errorDetails: any = null; // Aggiungi una variabile per i dettagli dell'errore
 
   constructor(private router: Router) {}
 
@@ -36,6 +39,31 @@ export class RegisterComponent {
         'Errore durante la registrazione:',
         error.response?.data || error.message || error
       );
+      this.errorMessage = 'Errore durante la registrazione';
+      this.errorDetails = error.response?.data || error.message || error; // Imposta i dettagli dell'errore
     }
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['/login']); // Redirect alla pagina /login
+  }
+
+  getErrorDetails(): string[] {
+    if (!this.errorDetails) {
+      return [];
+    }
+
+    const details: string[] = [];
+    for (const key in this.errorDetails) {
+      if (this.errorDetails.hasOwnProperty(key)) {
+        const value = this.errorDetails[key];
+        if (Array.isArray(value)) {
+          details.push(...value);
+        } else {
+          details.push(value);
+        }
+      }
+    }
+    return details;
   }
 }
