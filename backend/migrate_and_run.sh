6979b5c40f6f6304d810cae607407e103eb8b914
pietro.marilei <pietromrl0@gmail.com>
2024-08-09 12:44:46 +0200

    add create db

--- backend/migrate_and_run.sh
+++ backend/migrate_and_run.sh
@@ -3,11 +3,13 @@
 # Attendi che il database sia pronto
 ./wait-for-it.sh db:3306 --timeout=60 --strict -- echo "Database è pronto!"
 
+# Crea il database se non esiste
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" | mysql -h db -u root -p"${DB_PASSWORD}"
+
 # Esegui le migrazioni solo se necessario
 if [ "$DJANGO_MANAGEPY_MIGRATE" = 'on' ]; then
     echo "Applicazione delle migrazioni..."
     python manage.py migrate
-
 fi
 
 # Avvia il server Django
